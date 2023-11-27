07e798779c0664703fb5dc5ee4d62828a35cbd29
Andrewjsunwoo <Andrewjsunwoo@users.noreply.github.com>
2023-11-27 17:18:49 -0500

    backend done

---
+++ src/main/java/com/andrew/movies/repository/MovieRepository.java
@@ -0,0 +1,13 @@
+package com.andrew.movies.repository;
+
+import com.andrew.movies.model.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
+}
